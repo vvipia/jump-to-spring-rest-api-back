bc9d848f7d9e58363028cdafc7350b5dad03cd79
GIoriousPurpose <vvipia@naver.com>
2026-08-19 16:19:32 +0900

    3차 목표: HTTP상태코드 + ResponseEntity 적용

--- src/main/java/com/back/jumptospringwithgpt/question/controller/QuestionController.java
+++ src/main/java/com/back/jumptospringwithgpt/question/controller/QuestionController.java
@@ -6,6 +6,8 @@ import com.back.jumptospringwithgpt.question.dto.QuestionResponse;
 import com.back.jumptospringwithgpt.question.entity.Question;
 import com.back.jumptospringwithgpt.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,36 +21,42 @@ public class QuestionController {
 
     // 전체 질문 반환
     @GetMapping
-    public List<QuestionResponse> questions() {
-        return questionService.getQuestionList()
-                .stream()
-                .map(QuestionResponse::from) //Question객체를 하나씩 QuestionResponse로 변환
-                .toList();
+    public ResponseEntity<List<QuestionResponse>> questions() {
+        //Question객체를 하나씩 QuestionResponse로 변환
+        List<QuestionResponse> responses = questionService.getQuestionList()
+                                                .stream()
+                                                .map(QuestionResponse::from)
+                                                .toList();
+        return ResponseEntity.ok(responses); //상태 코드 200 OK + 데이터 보내기
     }
 
     // 특정 질문 반환
     @GetMapping("/{id}")
-    public QuestionResponse detail(@PathVariable Integer id) {
-        return QuestionResponse.from(questionService.getQuestion(id));
+    public ResponseEntity<QuestionResponse> detail(@PathVariable Integer id) {
+        QuestionResponse response = QuestionResponse.from(questionService.getQuestion(id));
+        return ResponseEntity.ok(response);
     }
 
     // 질문 생성
     @PostMapping
-    public QuestionResponse createQuestion(@RequestBody QuestionCreateRequest request) {
+    public ResponseEntity<QuestionResponse> createQuestion(@RequestBody QuestionCreateRequest request) {
         Question question = questionService.create(request.subject(), request.content());
-        return QuestionResponse.from(question);
+        QuestionResponse response = QuestionResponse.from(question);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     // 질문 수정
     @PatchMapping("/{id}")
-    public QuestionResponse modifyQuestion(@PathVariable Integer id,
-                                   @RequestBody QuestionModifyRequest request) {
-        return QuestionResponse.from(questionService.modify(id, request.subject(), request.content()));
+    public ResponseEntity<QuestionResponse> modifyQuestion(@PathVariable Integer id,
+                                                           @RequestBody QuestionModifyRequest request) {
+        QuestionResponse response = QuestionResponse.from(questionService.modify(id, request.subject(), request.content()));
+        return ResponseEntity.ok(response);
     }
 
     // 질문 삭제
     @DeleteMapping("/{id}")
-    public void deleteQuestion(@PathVariable Integer id) {
+    public ResponseEntity<Void> deleteQuestion(@PathVariable Integer id) {
         questionService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }

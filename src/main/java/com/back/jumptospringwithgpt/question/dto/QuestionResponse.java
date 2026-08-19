d229ddce5cb805f384bbee14a6f866e67c8e51bb
GIoriousPurpose <vvipia@naver.com>
2026-08-19 15:33:27 +0900

    1차 목표: 질문 컨트롤러 RestApi 방식으로 변경

--- src/main/java/com/back/jumptospringwithgpt/question/dto/QuestionResponse.java
+++ src/main/java/com/back/jumptospringwithgpt/question/dto/QuestionResponse.java
@@ -1,15 +1,16 @@
 package com.back.jumptospringwithgpt.question.dto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @AllArgsConstructor
-public class QuestionResponse {
-
-    private Integer id;
+public class QuestionCreateRequest {
+    @NotEmpty(message = "제목은 필수입니다.")
+    @Size(max = 200, message = "제목은 200자 이하로 작성해주세요.")
     private String subject;
+
+    @NotEmpty(message = "내용은 필수입니다.")
     private String content;
-    private LocalDateTime createDate;
 }
